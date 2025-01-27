22df1748117f875a09c4aac6a5e6356c474fc48e
Neo Rankapole <neorankapole24@gmail.com>
2025-01-27 19:36:34 +0200

    Fixed skills section light mode display

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,13 +1,19 @@
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from "../../assets/checkmark-dark.svg"
 import SkillList from "../../common/SkillList";
+import checkMarkIconDark from "../../assets/checkmark-dark.svg";
+import checkMarkIconLight from "../../assets/checkmark-light.svg";
+import { useTheme } from "../../common/ThemeContext";
+
+
 function Skills() {
+  const {theme} = useTheme();
+  const checkMarkIcon = theme === "light" ? checkMarkIconLight : checkMarkIconDark;
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skillName="HTML5"/>
         <SkillList src={checkMarkIcon} skillName="CSS"/>
+        <SkillList src={checkMarkIcon} skillName="HTML5"/>
         <SkillList src={checkMarkIcon} skillName="JavaScript"/>
         <SkillList src={checkMarkIcon} skillName="C#"/>
         <SkillList src={checkMarkIcon} skillName="Delphi"/>
