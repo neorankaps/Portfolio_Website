1f56b5abe264b355febe2672c2c91e6842f36301
Neo Rankapole <neorankapole24@gmail.com>
2025-01-27 19:44:40 +0200

    Improved readability

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -4,26 +4,26 @@ import checkMarkIconDark from "../../assets/checkmark-dark.svg";
 import checkMarkIconLight from "../../assets/checkmark-light.svg";
 import { useTheme } from "../../common/ThemeContext";
 
-
 function Skills() {
-  const {theme} = useTheme();
-  const checkMarkIcon = theme === "light" ? checkMarkIconLight : checkMarkIconDark;
+  const { theme } = useTheme();
+  const checkMarkIcon =
+    theme === "light" ? checkMarkIconLight : checkMarkIconDark;
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skillName="CSS"/>
-        <SkillList src={checkMarkIcon} skillName="HTML5"/>
-        <SkillList src={checkMarkIcon} skillName="JavaScript"/>
-        <SkillList src={checkMarkIcon} skillName="C#"/>
-        <SkillList src={checkMarkIcon} skillName="Delphi"/>
+        <SkillList src={checkMarkIcon} skillName="CSS" />
+        <SkillList src={checkMarkIcon} skillName="HTML5" />
+        <SkillList src={checkMarkIcon} skillName="JavaScript" />
+        <SkillList src={checkMarkIcon} skillName="C#" />
+        <SkillList src={checkMarkIcon} skillName="Delphi" />
       </div>
-      <hr/>
+      <hr />
       <div className={styles.skillList}>
-      <SkillList src={checkMarkIcon} skillName="Git"/>
-        <SkillList src={checkMarkIcon} skillName="Bootstrap"/>
-        <SkillList src={checkMarkIcon} skillName="NodeJS"/>
-        <SkillList src={checkMarkIcon} skillName="React"/>
+        <SkillList src={checkMarkIcon} skillName="Git" />
+        <SkillList src={checkMarkIcon} skillName="Bootstrap" />
+        <SkillList src={checkMarkIcon} skillName="NodeJS" />
+        <SkillList src={checkMarkIcon} skillName="React" />
       </div>
     </section>
   );
